c40968111710fb89e141e51f1db8ba1188b3e1eb
rafaelpradella <pradella@outlook.com>
2024-03-18 22:03:24 +0000

    feat: Create readme example with Astro repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [metaTags()]
+});
---
+++ src/constants/AstroGithub.ts
@@ -0,0 +1,3 @@
+export const RAW_GITHUB_URL = `https://raw.githubusercontent.com/withastro/astro/main`;
+
+export const GITHUB_ASSETS_PATH = `${RAW_GITHUB_URL}/.github/assets`;
