47fc79a69009f5cf5a90706e2789f82944bde583
rafaelpradella <pradella@outlook.com>
2024-03-19 15:22:23 +0000

    feat: Replace deploy CF to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import metaTags from "astro-meta-tags";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [metaTags(), react()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
