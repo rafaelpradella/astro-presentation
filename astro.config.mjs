751851149340c65a6fef65d22e3cabee0f13d353
rafaelpradella <pradella@outlook.com>
2024-03-19 15:06:27 +0000

    feat: Add Cloudflare deploy support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import metaTags from "astro-meta-tags";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [metaTags(), react()]
+  integrations: [metaTags(), react()],
+  output: "server",
+  adapter: cloudflare()
 });
