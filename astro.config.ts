ba726eacbe0362c85d7638cba638aa68fde55fa8
rafaelpradella <pradella@outlook.com>
2024-03-20 17:37:32 +0000

    fix: Replicate tsconfig alias in astro.config

---
+++ astro.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from "path";
+import { defineConfig } from "astro/config";
+import metaTags from "astro-meta-tags";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+
+import { default as abTestDirective } from "./src/utils/directives/abTest/register";
+import icon from "astro-icon";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const root = resolve(__dirname, 'src');
+
+const alias = {
+  "@assets": resolve(root, 'assets'),
+  "@constants": resolve(root, 'constants'),
+  "@content": resolve(root, 'content'),
+  "@components": resolve(root, 'components'),
+  "@layouts": resolve(root, 'layouts'),
+  "@pages": resolve(root, 'pages'),
+  "@utils": resolve(root, 'utils'),
+};
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [metaTags(), react(), icon(), abTestDirective()],
+  output: "server",
+  adapter: vercel(),
+  vite: {
+    resolve: { alias },
+  },
+});
