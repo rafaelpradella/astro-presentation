400175c4edb00771e9d97c3399051095e347055b
rafaelpradella <pradella@outlook.com>
2024-03-20 10:03:25 +0000

    fix: Hydration feedback for SharePost

--- src/pages/blogPost/components/SharePost.tsx
+++ src/pages/blogPost/components/SharePost.tsx
@@ -6,20 +6,25 @@ import css from "./SharePost.module.css";
 type TShareProps = { postUrl: string };
 
 export const SharePost: FC<TShareProps> = ({ postUrl }) => {
+  
+  const onSharePost = async(type: 'wpp' | 'twitter' | 'main') => {
+    if(type !== 'main') return alert(`Sharing ${postUrl} using ${type}`);
+    await window.navigator.share({ title: 'Share Title', text: 'Hydrated result', url: postUrl });
+  }
   return (
     <ul className={css.block}>
       <li>
-        <button className={css.button} aria-label="Share on Whatsapp">
+        <button onClick={() => onSharePost('wpp')} className={css.button} aria-label="Share on Whatsapp">
           <Icon className={css.icon} icon="basil:whatsapp-outline" />
         </button>
       </li>
       <li>
-        <button className={css.button} aria-label="Share on Twitter">
+        <button onClick={() => onSharePost('twitter')} className={css.button} aria-label="Share on Twitter">
           <Icon className={css.icon} icon="basil:twitter-outline" />
         </button>
       </li>
       <li>
-        <button className={css.button} aria-label="Share Post">
+        <button onClick={() => onSharePost('main')} className={css.button} aria-label="Share Post">
           <Icon className={css.icon} icon="basil:share-outline" />
         </button>
       </li>
